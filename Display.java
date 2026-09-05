7a81c3c706ab5c5a8f4515cf897eafb1c5ff29fa
Brajagopaldas <55945388+Brajagopaldas@users.noreply.github.com>
2026-09-05 21:13:40 +0530

    Add comment for displayStudent method
    Added a comment to explain the purpose of the displayStudent method.

--- Display.java
+++ Display.java
@@ -1,3 +1,5 @@
+// Q4 .  Write a Java program to create a method named displayStudent() that accepts the student's name, age, and marks as parameters. Display all the values received by the method.
+
 public class Display {
    static void displayStudent(String name, int age, int mark){
    System.out.println("Name: "+ name);
