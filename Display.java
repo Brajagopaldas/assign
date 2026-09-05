2e070acc0e6ead8b1401e06fa66b656a004a77a3
Brajagopaldas <55945388+Brajagopaldas@users.noreply.github.com>
2026-09-05 20:15:01 +0530

    Add files via upload

---
+++ Display.java
@@ -0,0 +1,10 @@
+public class Display {
+   static void displayStudent(String name, int age, int mark){
+   System.out.println("Name: "+ name);
+   System.out.println("Age: "+ age);
+   System.out.println("Mark: "+ mark);
+   }
+   public static void main(String[] args){
+    displayStudent("Braja", 24, 97);
+   }
+}
