09a1735af6ba1c739be13b6ef037fb0c66df02d3
Brajagopaldas <55945388+Brajagopaldas@users.noreply.github.com>
2026-09-05 18:45:29 +0530

    Add files via upload

---
+++ .github/workflows/Rectangle.java
@@ -0,0 +1,10 @@
+class Rectangle {
+    static void calculateArea(int length, int breadth){
+        int area = length * breadth;
+        System.out.println("Area: " + area);
+    }
+    public static void main(String[] args){
+        calculateArea(25, 30);
+    }
+        
+}
