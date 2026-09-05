60a30d79091e938ab72d0556f36e6cdc060323be
Brajagopaldas <55945388+Brajagopaldas@users.noreply.github.com>
2026-09-05 21:06:32 +0530

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,3 +1,5 @@
+// Q.7  Write a Java program to create a method named calculateArea() that accepts length and breadth as parameters. Calculate the area of the rectangle using a local variable and display the result.
+
 class Rectangle {
     static void calculateArea(int length, int breadth){
         int area = length * breadth;
