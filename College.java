5eff50ab6cb3e75a62079429147c9a7ef011c5b7
Brajagopaldas <55945388+Brajagopaldas@users.noreply.github.com>
2026-09-05 20:16:05 +0530

    Add files via upload

---
+++ College.java
@@ -0,0 +1,6 @@
+public class College {
+    static String collegeName = "Seemanta Engineering College";
+    public static void main(String[] args){
+        System.out.println("College Name: "+ collegeName);
+    }
+}
