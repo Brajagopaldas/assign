9079ca51cc7f90d6e9edc30fdacd2040409a92ce
Brajagopaldas <55945388+Brajagopaldas@users.noreply.github.com>
2026-09-05 19:01:33 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    String name = "Braja";
+    int rollNo = 136;
+    int mark = 98;
+    public static void main(String[] args){
+        Student s = new Student();
+        System.out.println(s.name);
+        System.out.println(s.rollNo);
+        System.out.println(s.mark);
+    }   
+}
