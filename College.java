1cd421b1423598b9890f951534356e3a76f8a134
Brajagopaldas <55945388+Brajagopaldas@users.noreply.github.com>
2026-09-05 21:00:43 +0530

    Update College.java

--- College.java
+++ College.java
@@ -1,3 +1,5 @@
+// Q.2 Write a Java program to declare collegeName as a static variable inside a Student class. Assign a suitable value to the static variable and display the college name
+
 public class College {
     static String collegeName = "Seemanta Engineering College";
     public static void main(String[] args){
